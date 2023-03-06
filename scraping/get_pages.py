5f571aca2705c08800396597269ffbbb3efb44cb
katerina vaseva <katerina.vsv@gmail.com>
2023-03-06 16:56:43 +0100

    clean up

--- scraping/get_pages.py
+++ scraping/get_pages.py
@@ -1,15 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-import json
-import os
-import re
 from utils import write_json
 
 page = requests.get("https://famouspeople.astro-seek.com/filter_date/?umrti=&asc=&narozeni_den=nezalezi&narozeni_mesic=nezalezi&narozeni_rok=&narozeni_stat=&pohlavi=&razeni=datum_vek_sestupne")
 soup = BeautifulSoup(page.content, 'html.parser')
-
-current_dir = os.path.abspath(os.curdir)
-dir_path = os.path.abspath(os.path.join(current_dir, os.pardir))
 
 def get_pagination_links():
     pg_links = []
